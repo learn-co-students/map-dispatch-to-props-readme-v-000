13da105aa5cda681ae821f9d49f4d210891c95da
Christian Egglin <cegglin@conncoll.edu>
2018-04-08 23:48:35 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import { bindActionCreators } from  'redux';
+import { addItem } from  './actions/items';
 
 class App extends Component {
 
