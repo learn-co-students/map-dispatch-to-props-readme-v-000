c670d2e249f6eea276ff40cc5a52ee98098191df
ayesha-x-ansari <ayesha-x-ansari@github.com>
2018-10-12 20:26:28 -0700

    added code

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
+  debugger;
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -2,12 +2,15 @@ export default function shoppingListItemReducer(state = {
   items: []
 }, action) {
   console.log(action)
+  console.log(state)
   switch(action.type) {
 
     case 'INCREASE_COUNT':
       console.log(state.items.concat(state.items.length));
+      debugger;
       return Object.assign({}, {
         items: state.items.concat(state.items.length)
+  
       });
 
     default:
