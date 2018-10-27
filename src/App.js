a8653ff3109d8f7af1958d9f2a6371004f174373
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-10-27 19:14:33 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import { addItem } from  './actions/items';
-
+ 
 class App extends Component {
-
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
+ 
+  handleOnClick = event => {
+    this.props.addItem()
   }
-
+ 
   render() {
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
+        <button onClick={this.handleOnClick}>
           Click
           </button>
         <p>{this.props.items.length}</p>
@@ -20,11 +20,19 @@ class App extends Component {
     );
   }
 };
-
+ 
 const mapStateToProps = (state) => {
   return {
     items: state.items
   };
 };
-
-export default connect(mapStateToProps)(App);
+ 
+const mapDispatchToProps = dispatch => {
+  return {
+    addItem: () => {
+      dispatch(addItem())
+    }
+  };
+};
+ 
+export default connect(mapStateToProps, mapDispatchToProps)(App);
