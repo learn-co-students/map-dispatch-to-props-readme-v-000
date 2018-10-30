79b8f56862f6278489684cd284b0894546f1917f
Su0414 <susanaik.com>
2018-10-30 09:27:24 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
   }
 
   render() {
+    debugger;
     return (
       <div className="App">
         <button onClick={(event) => this.handleOnClick(event)}>
@@ -21,10 +22,18 @@ class App extends Component {
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     items: state.items
+//   };
+// };
 
-export default connect(mapStateToProps)(App);
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addItem: () => {
+//       dispatch(addItem())
+//     }
+//   };
+// };
+
+export default connect(state => ({ items: state.items }))(App);
