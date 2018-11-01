2db106294f236e6aa17a7b6755fac02b0862ca41
rfenix3 <richard.fenix@gmail.com>
2018-11-01 08:56:02 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const store = createStore(shoppingListItemReducer, window.__REDUX_DEVTOOLS_EXTEN
 
 ReactDOM.render(
   <Provider store={store}>
-    <App store={store}/>
+    <App />
    </Provider>
   ,document.getElementById('root')
 );
