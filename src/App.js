2a44af564660ee59db5b4d9045106ed08e95eec8
CSavi <carrielane89@gmail.com>
2018-11-03 10:48:57 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import { addItem } from  './actions/items';
 
 class App extends Component {
 
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
+  handleOnClick = event => {
+    this.props.addItem()
   }
 
   render() {
+    debugger
     return (
       <div className="App">
         <button onClick={(event) => this.handleOnClick(event)}>
@@ -21,10 +22,25 @@ class App extends Component {
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     items: state.items
+//   };
+// };
 
-export default connect(mapStateToProps)(App);
+// // the dispatch function is available as an argument; everything we need into a single prop value
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addItem: () => {
+//       dispatch(addItem())
+//     }
+//   };
+// };
+
+// ES6 shorthand lets us pass in one value that will be read as the key and value
+export default connect(state => ({ items: state.items }), { addItem })(App);
+
+// OR
+
+// // if no specified 2nd arg to connect, dispatch is auto injected as a prop in component
+// export default connect(state => ({ items: state.items }))(App);
