02304bea2739fd9dc44cea529d266468115bd4bc
Deborah Seow <deborahseow9@gmail.com>
2018-11-08 16:57:33 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import { addItem } from  './actions/items';
 class App extends Component {
 
   handleOnClick() {
-    this.props.store.dispatch(addItem());
+    //this.props.store.dispatch(addItem()); changes to:
+    this.props.addItem()
   }
 
   render() {
+    debugger
     return (
       <div className="App">
         <button onClick={(event) => this.handleOnClick(event)}>
@@ -27,4 +29,22 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(App);
+//TOGETHER//
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addItem: () => {
+//       dispatch(addItem())
+//     }
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+//TOGETHER//
+
+export default connect(mapStateToProps, { addItem })(App);
+/*ES6 shorthand lets us pass in *one* value that will be read as the key and value.
+The second argument of connect will accept a function (as we've seen) or an object. If we pass in a function, mapDispatchToProps(), we must incorporate dispatch. If we pass in an object, connect handles this for us!
+*/
+
+/* Can delete both maptoprops functions: export default connect(state => ({ items: state.items }), { addItem })(App);
+*/
