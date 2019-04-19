fc935bb59fc6712cfbe16f381f2695e990d53213
nichia <nichia@gmail.com>
2019-04-19 17:59:47 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
-import { addItem } from  './actions/items';
+import { addItem } from './actions/items';
 
 class App extends Component {
-
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
-  }
+  handleOnClick = event => {
+    this.props.addItem();
+  };
+  // handleOnClick() {
+  //   this.props.store.dispatch(addItem());
+  // }
 
   render() {
+    debugger;
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
+        <button onClick={this.handleOnClick}>
+          {/* <button onClick={(event) => this.handleOnClick(event)}> */}
           Click
-          </button>
+        </button>
         <p>{this.props.items.length}</p>
       </div>
     );
   }
-};
+}
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
-};
+// const mapStateToProps = state => {
+//   return {
+//     items: state.items
+//   };
+// };
 
-export default connect(mapStateToProps)(App);
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addItem: () => {
+//       dispatch(addItem());
+//     }
+//   };
+// };
+
+export default connect(
+  // mapStateToProps,
+  // mapDispatchToProps
+  state => ({ items: state.items }),
+  { addItem }
+)(App);
