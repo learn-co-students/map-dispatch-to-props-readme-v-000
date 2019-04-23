b81b559abea97481e2c0048248c3496c2b43f4c8
julianjohannesen <github email address>
2019-04-23 13:54:42 -0400

    Done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,21 @@ import { addItem } from  './actions/items';
 class App extends Component {
 
   handleOnClick() {
-    this.props.store.dispatch(addItem());
+    // Replace the call to this.props.store.dispatch(addItem())
+    // with a call to this.props.addItem() by supplying a 2nd
+    // argument to connect() below. The 2nd item is an object
+    // containing addItem (which we imported above). connect()
+    // will automatically wrap addItem in a call to dispatch,
+    // e.g. dispatch(addItem())
+
+    // Anyway, once that's done, we can remove the store attribute
+    // from <App store={store} /> in index.js
+
+    // This is the old line:
+    // this.props.store.dispatch(addItem());
+
+    // And this is the new line:
+    this.props.addItem();
   }
 
   render() {
@@ -14,7 +28,7 @@ class App extends Component {
       <div className="App">
         <button onClick={(event) => this.handleOnClick(event)}>
           Click
-          </button>
+        </button>
         <p>{this.props.items.length}</p>
       </div>
     );
@@ -27,4 +41,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, { addItem })(App);
