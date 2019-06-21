d595e273b9dca66dc97316a43c0eb97968ac362d
harleyharl <harleymteden@gmail.com>
2019-06-21 11:48:16 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import { addItem } from  './actions/items';
 
 class App extends Component {
 
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
+  handleOnClick = event => {
+    this.props.addItem();
   }
 
   render() {
+    debugger
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
+        <button onClick={this.handleOnClick}>
           Click
           </button>
         <p>{this.props.items.length}</p>
@@ -27,4 +28,19 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(App);
+// see *was below,this goes with the old pre-es6 way
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addItem: () => {
+//       dispatch(addItem())
+//     }
+//   };
+// };
+
+
+export default connect(mapStateToProps, {addItem})(App);
+// *was:
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+// could go as far as:
+// export default connect(state => ({ items: state.items }), { addItem })(App);
+// By default mapDispatchToProps is just dispatch => ({ dispatch }). So if you don't specify the second argument to connect(), you'll get dispatch injected as a prop in your component.
