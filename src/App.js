cbc3e7bb8bb11811c7e97b6880eed4be40f8aacf
mat_anderson <matandersonlv@hotmail.com>
2020-07-09 14:37:06 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import { addItem } from  './actions/items';
-
+ 
 class App extends Component {
-
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
+ 
+  handleOnClick = event => {
+    this.props.addItem()
   }
-
+ 
   render() {
+    debugger
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
+        <button onClick={this.handleOnClick}>
           Click
           </button>
         <p>{this.props.items.length}</p>
@@ -20,11 +21,24 @@ class App extends Component {
     );
   }
 };
+ 
+// const mapStateToProps = state => {
+//   return {
+//     items: state.items
+//   }
+// }
+ 
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addItem: () => { dispatch(addItem()) }
+//   }
+// }
+ 
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
-};
+export default connect(state => ({ items: state.items }), { addItem })(App); // Equivalent to commented out code above
 
-export default connect(mapStateToProps)(App);
+// export default connect(state => ({ items: state.items }))(App); // Eqivalent to above because:
+
+// By default mapDispatchToProps is just dispatch => ({ dispatch }). 
+// So if you don't specify the second argument to connect(), you'll get dispatch injected as a prop in your component.
