4bbb666c3d3380aa9dd5a0890402d87439f46386
tscotce <tatyana.celovsky@gmail.com>
2021-03-23 17:38:04 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -27,16 +27,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-// Code change: this new function takes in dispatch as an argument
-// It then returns an object that contains a function as a value!
-// Notice above in handleOnClick() that this function, addItem(),
-// is what is called, NOT the addItem action creator itself.
-const mapDispatchToProps = dispatch => {
-  return {
-    addItem: () => {
-      dispatch(addItem())
-    }
-  };
-};
- 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, { addItem })(App); // Code change: no mapDispatchToProps function required!
