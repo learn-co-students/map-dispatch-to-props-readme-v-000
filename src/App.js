acfb1aaebd89938ec7054e1f1edaf42fae20c562
yurigurlie207 <adrienne.yao@gmail.com>
2021-03-28 14:39:31 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import { addItem } from  './actions/items';
-
+ 
 class App extends Component {
-
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
+ 
+  handleOnClick = event => {
+    this.props.addItem()
   }
-
+ 
   render() {
+    debugger
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
+        <button onClick={this.handleOnClick}>
           Click
           </button>
         <p>{this.props.items.length}</p>
@@ -20,11 +21,29 @@ class App extends Component {
     );
   }
 };
+ 
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
-};
+//only need this as one line, or all of the below
+export default connect(state => ({ items: state.items }), { addItem })(App);
 
-export default connect(mapStateToProps)(App);
+
+// OR 
+// const mapStateToProps = (state) => {
+//   return {
+//     items: state.items
+//   };
+// };
+
+// const mapStateToProps = state => {
+//   return {
+//     items: state.items
+//   }
+// }
+ 
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addItem: () => { dispatch(addItem()) }
+//   }
+// }
+ 
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
